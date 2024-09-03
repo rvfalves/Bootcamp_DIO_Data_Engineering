ca33b243dce7f08425e59dfbd1e642ecbdf9ede8
Rafael Alves <rvf_alves@yahoo.com.br>
2024-09-03 22:31:47 +0100

    Final version of project1.py
    Final version of project1.py

--- project1.py
+++ project1.py
@@ -1,5 +1,6 @@
 menu = """
 
+Escolha a operação de deseja executar:
 [d] Depositar
 [s] Sacar
 [e] Extrato
@@ -7,26 +8,53 @@ menu = """
 
 => """
 
-saldo = 0
-limite = 500
-extrato = ""
-numero_saques = 0
-LIMITE_SAQUES = 3
+balance = 0
+limit = 500
+bank_statement = ""
+withdraw_number = 0
+WITHDRAW_LIMIT = 3
 
 while True:
 
-    opcao = input(menu)
+    option = input(menu)
 
-    if opcao == "d":
-       
+    if option == "d":
+        deposit = float(input("Informe o valor deseja depositar: "))
+        if deposit > 0:
+            balance += deposit
+            bank_statement += f"Depósito de R$ {deposit:.2f}\n"
+            print(f"Depósito de R$ {deposit:.2f} realizado com sucesso.")
+        else:
+            print("Erro. O valor a ser depositado deve ser um número maior que zero.")
 
-    elif opcao == "s":
-        
+    elif option == "s":
+        if withdraw_number < WITHDRAW_LIMIT:
+            withdraw = float(input("Informe o valor deseja sacar: "))
+            if withdraw >0:
+                if withdraw <= limit:
+                    if withdraw <= balance:
+                        balance -= withdraw
+                        bank_statement += f"Saque de R$ {withdraw:.2f}\n"
+                        print(f"Saque de R$ {withdraw:.2f} realizado com sucesso.")
+                        withdraw_number += 1
+                    else:
+                        print(f"Lamentamos, mas o valor de saque desejado é maior que o seu saldo de R$ {balance:.2f}.")
+                else:
+                    print(f"Lamentamos, mas você excedeu o limite de R$ {limit:.2f} por saque.")
+            else:
+                print("Erro. O valor a ser sacado deve ser um número maior que zero.")
+        else:
+            print("Lamentamos, mas você excedeu o número de saques diários")
 
-    elif opcao == "e":
-       
+    elif option == "e":
+       print("\nEXTRATO DE CONTA")
+       if bank_statement == "":
+           print("Não foram realizadas movimentações")
+       else:
+           print(bank_statement)
+           print(f"\nSaldo de conta: R$ {balance:.2f}")
 
-    elif opcao == "q":
+    elif option == "q":
         break
 
     else:
