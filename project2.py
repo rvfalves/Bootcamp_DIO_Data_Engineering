0f42ee17baa6809a73149714caf1905946cba130
Rafael Alves <rvf_alves@yahoo.com.br>
2024-09-18 01:11:37 +0100

    Project2_v2
    Final version of Project2

--- project2.py
+++ project2.py
@@ -1,24 +1,24 @@
 
-def deposit(balance, value, bank_statement, /):
+def deposit(account, value, /):
     if value > 0:
-        balance += value
-        bank_statement += f"Depósito de R$ {value:.2f}\n"
-        print(f"Depósito de R$ {value:.2f} realizado com sucesso.")
+        account["balance"] += value
+        account["bank_statement"] += f"Depósito de R$ {value:.2f}\n"
+        print(f"Depósito de R$ {value:.2f} realizado com sucesso à conta {account["account_number"]}")
     else:
         print("Erro. O valor a ser depositado deve ser um número maior que zero.")
-    return balance, bank_statement
+    return account
 
-def withdraw(*, balance, value, bank_statement, limit, withdraw_number, withdraw_limit):
-    if withdraw_number < withdraw_limit:
+def withdraw(*, account, value, limit, withdraw_limit):
+    if account["withdraw_number"] < withdraw_limit:
         if value >0:
             if value <= limit:
-                if value <= balance:
-                    balance -= value
-                    bank_statement += f"Saque de R$ {value:.2f}\n"
-                    print(f"Saque de R$ {value:.2f} realizado com sucesso.")
-                    withdraw_number += 1
+                if value <= account["balance"]:
+                    account["balance"] -= value
+                    account["bank_statement"] += f"Saque de R$ {value:.2f}\n"
+                    print(f"Saque de R$ {value:.2f} realizado com sucesso à conta {account["account_number"]}.")
+                    account["withdraw_number"] += 1
                 else:
-                    print(f"Lamentamos, mas o valor de saque desejado é maior que o seu saldo de R$ {balance:.2f}.")
+                    print(f"Lamentamos, mas o valor de saque desejado é maior que o seu saldo de R$ {account["balance"]:.2f}.")
             else:
                 print(f"Lamentamos, mas você excedeu o limite de R$ {limit:.2f} por saque.")
         else:
@@ -26,15 +26,16 @@ def withdraw(*, balance, value, bank_statement, limit, withdraw_number, withdraw
     else:
         print("Lamentamos, mas você excedeu o número de saques diários")
     
-    return withdraw_number, balance, bank_statement
+    return account
 
-def statement(balance,/,*, bank_statement):
+def statement(*, account):
     print("\nEXTRATO DE CONTA")
-    if bank_statement == "":
+    print(f"Conta de número: {account["account_number"]}")
+    if account["bank_statement"] == "":
         print("Não foram realizadas movimentações")
     else:
-        print(bank_statement)
-        print(f"\nSaldo de conta: R$ {balance:.2f}")
+        print(account["bank_statement"])
+        print(f"\nSaldo de conta: R$ {account["balance"]:.2f}")
 
 def register_user(cpf, name, birth_date, address, users):
     for user in users:
@@ -47,32 +48,32 @@ def register_user(cpf, name, birth_date, address, users):
                 "birth_date": birth_date,
                 "address": address
             })
-    print("Cliente cadastrado com sucesso.")
+    print(f"Cliente {users[-1]["name"]} cadastrado com sucesso.")
     return users
 
 def create_account(cpf, users, accounts):
     for user in users:
         if cpf == user["cpf"]:
             accounts.append({
-                "agencia": "0001",
-                "numero_conta": len(accounts)+1,
+                "agency": "0001",
+                "account_number": len(accounts)+1,
                 "user": user,
                 "balance": 0,
                 "bank_statement": "",
                 "withdraw_number": 0
             })
-            print("Conta cadastrada com sucesso.")
+            print(f"Conta número {accounts[-1]["account_number"]} cadastrada com sucesso.")
             return accounts
     print("Este CPF não foi cadastrado. Efetue o cadastro de usuário antes de cadastrar sua conta.")
     return accounts
 
 def list_users(users):
     for user in users:
-        print("\n CPF: {user[cpf]} \n Nome: {user[name]} \n Data de nascimento: {user[birth_date]} \n Endereço: {user[address]}")
+        print(f"\n CPF: {user["cpf"]} \n Nome: {user["name"]} \n Data de nascimento: {user["birth_date"]} \n Endereço: {user["address"]}")
 
 def list_accounts(accounts):
     for account in accounts:
-        print("\n Agência: {account[agency]} \n Conta-corrente: {account[account_number]} \n Cliente: {account[user][name]} \n CPF: {account[user][cpf]}")
+        print(f"\n Agência: {account["agency"]} \n Conta-corrente: {account["account_number"]} \n Cliente: {account["user"]["name"]} \n CPF: {account["user"]["cpf"]}")
 
 menu = """
 
@@ -102,15 +103,26 @@ while True:
 
     if option == "d":
         account_number = int(input("Informe o número da sua conta: "))
-        value = float(input("Informe o valor deseja depositar: "))
-        balance, bank_statement = deposit(balance, value, bank_statement)
+        if account_number < 1 or account_number > len(accounts):
+            print("Conta-corrente não encontrada. Por favor, cadastre uma conta.")
+        else:
+            value = float(input("Informe o valor deseja depositar: "))
+            accounts[account_number-1] = deposit(accounts[account_number-1], value)
 
     elif option == "s":
-        value = float(input("Informe o valor deseja sacar: "))
-        withdraw_number, balance, bank_statement = withdraw(balance=balance, value=value, bank_statement=bank_statement, limit=limit, withdraw_number=withdraw_number, withdraw_limit=WITHDRAW_LIMIT)
+        account_number = int(input("Informe o número da sua conta: "))
+        if account_number < 1 or account_number > len(accounts):
+            print("Conta-corrente não encontrada. Por favor, cadastre cliente e conta.")
+        else:
+            value = float(input("Informe o valor deseja sacar: "))
+            accounts[account_number-1] = withdraw(account=accounts[account_number-1], value=value, limit=limit, withdraw_limit=WITHDRAW_LIMIT)
 
     elif option == "e":
-        statement(balance, bank_statement=bank_statement)
+        account_number = int(input("Informe o número da sua conta: "))
+        if account_number < 1 or account_number > len(accounts):
+            print("Conta-corrente não encontrada. Por favor, cadastre cliente e conta.")
+        else:
+            statement(account=accounts[account_number-1])
    
     elif option == "c":
         cpf = input("Informe o seu CPF (apenas números): ")
